66f47574145b3166ceabc3d2b90a6b2fced8836a
kuboshizuma <forothers.295@gmail.com>
2015-11-11 16:04:47 +0900

    Add item_database

---
+++ spec/item_database_spec.rb
@@ -0,0 +1,19 @@
+require 'shopping.rb'
+RSpec.describe(ItemDatabase) do
+  describe '#search' do
+    let(:item) { Item.new('item_name', 100) }
+    context 'with search item' do
+      it 'returns item' do
+        subject.register(item)
+        search_item = subject.search('item_name')
+        expect(search_item).to eq(item)
+      end
+    end
+    context 'without search item' do
+      it 'returns nil' do
+        search_item = subject.search('item_name')
+        expect(search_item).to eq(nil)
+      end
+    end
+  end
+end
